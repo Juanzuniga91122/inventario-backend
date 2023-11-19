93a259c423d5c38f00805059d886712e3b188baf
juanzuniga <juanzuniga@unicomfacauca.edu.co>
2023-11-18 21:57:14 -0500

    servicio actualizar categoria

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,48 +17,64 @@ import com.company.inventory.services.ICategoryService;
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
-	
+
 	@Autowired
 	private ICategoryService service;
-	
+
 	/**
-	 * get all the categories 
+	 * get all the categories
+	 * 
 	 * @return
 	 */
-	
+
 	@GetMapping("/categories")
-	public ResponseEntity<CategoryResponseRest> searchCategories(){
-		
-		ResponseEntity<CategoryResponseRest>  response = service.search();
+	public ResponseEntity<CategoryResponseRest> searchCategories() {
+
+		ResponseEntity<CategoryResponseRest> response = service.search();
 		return response;
-	
+
 	}
+
 	/**
 	 * get categories by id
+	 * 
 	 * @param id
 	 * @return
 	 */
-	
+
 	@GetMapping("/categories/{id}")
-	public ResponseEntity<CategoryResponseRest> searchByid(@PathVariable Long id){
-		
-		ResponseEntity<CategoryResponseRest>  response = service.searchById(id);
+	public ResponseEntity<CategoryResponseRest> searchByid(@PathVariable Long id) {
+
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
 		return response;
-	
+
 	}
-	
+
 	/**
 	 * save categories by id
+	 * 
 	 * @param Category
 	 * @return
 	 */
-	
+
 	@PostMapping("/categories")
-	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
-		
-		ResponseEntity<CategoryResponseRest>  response = service.save(category);
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category) {
+
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
 		return response;
-	
+
+	}
+	/**
+	 * update categories
+	 * @param category
+	 * @return
+	 */
+	@PutMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id) {
+
+		ResponseEntity<CategoryResponseRest> response = service.update(category, id);
+		return response;
+
 	}
 
 }
